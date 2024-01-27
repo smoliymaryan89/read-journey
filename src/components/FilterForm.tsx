914b8d518b33629eeff8fc7fa38725816a63a35f
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-27 12:53:57 +0200

    feat: create filter form

---
+++ src/components/FilterForm.tsx
@@ -0,0 +1,66 @@
+import { Formik } from "formik";
+import Input from "./ui/Input";
+import Button from "./ui/Button";
+
+export interface FilterData {
+  title: string;
+  author: string;
+}
+
+interface FilterFormProps {
+  handleFilter: (data: FilterData) => void;
+}
+
+const FilterForm = ({ handleFilter }: FilterFormProps) => {
+  return (
+    <Formik
+      initialValues={{ title: "", author: "" }}
+      onSubmit={({ title, author }: FilterData) => {
+        handleFilter({ title: title.trim(), author: author.trim() });
+      }}
+    >
+      {({ values, handleChange, handleSubmit }) => (
+        <>
+          <form
+            onSubmit={handleSubmit}
+            noValidate
+            className="mb-[20px] flex-1 md:mb-0"
+          >
+            <p className="mb-[8px] ml-[14px] text-10 leading-[1.2] tracking-[-0.2px] md:text-14 md:leading-[1.29] md:tracking-[-0.28px]">
+              Filters:
+            </p>
+            <Input
+              id={"title"}
+              type="text"
+              name="title"
+              value={values.title}
+              onChange={handleChange}
+              label="Book title:"
+              inputStyles="pl-[77px] pr-[14px] md:pl-[86px]"
+              wrapperStyles="mb-[8px]"
+            />
+            <Input
+              id={"author"}
+              type="text"
+              name="author"
+              value={values.author}
+              onChange={handleChange}
+              label="The author:"
+              inputStyles="pl-[85px] pr-[14px] md:pl-[95px]"
+              wrapperStyles="mb-[20px]"
+            />
+
+            <Button
+              type="submit"
+              title="To apply"
+              primary={false}
+              className="!h-[38px] px-[20px] md:!h-[42px] md:px-[28px] md:!text-16 md:!leading-[1.13] md:tracking-[0.32px]"
+            />
+          </form>
+        </>
+      )}
+    </Formik>
+  );
+};
+
+export default FilterForm;
