e436f73a0bf48c09bb891e20e87d8b4e975aef08
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-27 12:53:16 +0200

    feat: create book pagination

---
+++ src/components/BookPagination.tsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+import { BtnType } from "../types/pagination";
+
+interface BookPaginationProps {
+  page: number;
+  totalPages: number;
+  handlePage: (type: BtnType) => void;
+}
+
+const BookPagination = ({
+  page,
+  handlePage,
+  totalPages,
+}: BookPaginationProps) => {
+  return (
+    <ul className="flex items-center gap-[8px]">
+      <li>
+        <button
+          className="w-[32px] h-[32px] border border-grey-20 rounded-full flex items-center justify-center md:w-[40px] md:h-[40px]"
+          disabled={page === 1}
+          onClick={() => handlePage(BtnType.Prev)}
+        >
+          <svg
+            className={clsx(
+              "fill-transparent md:w-[20px] md:h-[40px]",
+              page === 1 ? "stroke-grey-20" : "stroke-light-white"
+            )}
+            width="16"
+            height="16"
+          >
+            <use href="./src/assets/icons/sprite.svg#icon-prev"></use>
+          </svg>
+        </button>
+      </li>
+      <li>
+        <button
+          className="w-[32px] h-[32px] border border-grey-20 rounded-full flex items-center justify-center md:w-[40px] md:h-[40px]"
+          disabled={totalPages === page}
+          onClick={() => handlePage(BtnType.Next)}
+        >
+          <svg
+            className={clsx(
+              "fill-transparent md:w-[20px] md:h-[40px]",
+              totalPages === page ? "stroke-grey-20" : "stroke-light-white"
+            )}
+            width="16"
+            height="16"
+          >
+            <use href="./src/assets/icons/sprite.svg#icon-next"></use>
+          </svg>
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+export default BookPagination;
