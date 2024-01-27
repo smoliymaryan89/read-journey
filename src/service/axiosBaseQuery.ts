960ba511682dffc2403dfdfdecfe103b75788231
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-27 12:13:29 +0200

    feat: create axios base query

---
+++ src/service/axiosBaseQuery.ts
@@ -0,0 +1,40 @@
+import { AxiosError, AxiosRequestConfig } from "axios";
+import { BaseQueryFn } from "@reduxjs/toolkit/query";
+import { instance } from "./axiosInstance";
+import { setAuthHeader } from "./axiosHeader";
+import { AuthState as State } from "types/auth";
+
+interface AuthState {
+  auth: State;
+}
+
+export const axiosBaseQuery: BaseQueryFn<{
+  url: string;
+  method: AxiosRequestConfig["method"];
+  data?: AxiosRequestConfig["data"];
+  params?: AxiosRequestConfig["params"];
+}> = async ({ url, method, data, params }, { getState }) => {
+  const token = (getState() as AuthState).auth.user.token;
+
+  if (token) {
+    setAuthHeader(token);
+  }
+
+  try {
+    const result = await instance({
+      url,
+      method,
+      data,
+      params,
+    });
+    return { data: result.data };
+  } catch (axiosError) {
+    const err = axiosError as AxiosError;
+    return {
+      error: {
+        status: err.response?.status,
+        data: err.response?.data || err.message,
+      },
+    };
+  }
+};
