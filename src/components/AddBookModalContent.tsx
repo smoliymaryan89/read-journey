43736a2aae87d250f9215b606a9573fcf15ebab7
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-27 19:15:15 +0200

    feat: create component for add book modal

---
+++ src/components/AddBookModalContent.tsx
@@ -0,0 +1,18 @@
+const AddBookModalContent = () => {
+  return (
+    <>
+      <p className="text-50 leading-none tracking-[-1px] mb-[20px] md:text-68 md:leading-[1.03] md:tracking-[-1.36px] md:mb-[32px]">
+        👍
+      </p>
+      <h2 className="text-18 font-gilroy-bold leading-none tracking-[-0.36px] mb-[10px] md:text-20 md:tracking-[-0.4px] md:mb-[14px]">
+        Good job
+      </h2>
+      <p className="text-grey text-14 leading-[1.29] tracking-[-0.28px] text-center">
+        Your book is now in <span>the library!</span> The joy knows no bounds
+        and now you can start your training
+      </p>
+    </>
+  );
+};
+
+export default AddBookModalContent;
