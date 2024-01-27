c503ca0131d0fcc8d33cf14f10fc9d731017675b
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-27 21:21:41 +0200

    feat: add redux provider to project

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "@store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
