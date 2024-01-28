08d75a8f93e68d4a6727e3e097409d50bea896d8
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-28 17:11:18 +0200

    feat: create func format time

---
+++ src/utils/helpers/formatTime.ts
@@ -0,0 +1,10 @@
+const formatTime = (timeString: string): string => {
+  const date = new Date(timeString);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${day}.${month}.${year}`;
+};
+
+export default formatTime;
