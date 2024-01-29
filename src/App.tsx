5ce8112997d0b306bf93142b37fb03816164236a
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-29 21:22:59 +0200

    feat: create app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,83 @@
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useAppDispatch } from "@hooks/useRedux";
+import { refreshUser } from "@store/auth/authOperations";
+
+import Layout from "@layouts/Layout";
+import RecommendedPage from "@pages/RecommendedPage";
+import LibraryPage from "@pages/LibraryPage";
+import ReadingPage from "@pages/ReadingPage";
+import LoginPage from "@pages/LoginPage";
+import RegistrationPage from "@pages/RegistrationPage";
+
+import PrivateRoute from "@guards/PrivateRoute";
+import PublicRoute from "@guards/PublicRoute";
+
 const App = () => {
-  return <div>App</div>;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <PrivateRoute>
+                <RecommendedPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/library"
+            element={
+              <PrivateRoute>
+                <LibraryPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/reading"
+            element={
+              <PrivateRoute>
+                <ReadingPage />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <RegistrationPage />
+            </PublicRoute>
+          }
+        />
+      </Routes>
+
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          duration: 3000,
+          className: "bg-dark-grey text-current",
+        }}
+      />
+    </>
+  );
 };
 
 export default App;
