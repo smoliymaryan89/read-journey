271e8d16b321feeb70de12a4653e241ca1407de5
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-30 00:42:46 +0200

    fix: fix img import

--- src/components/AppPreview.tsx
+++ src/components/AppPreview.tsx
@@ -4,30 +4,30 @@ const AppPreview = () => {
       <picture className="self-end">
         <source
           media="(min-width: 1440px)"
-          srcSet="./src/assets/images/auth/phone_desk@1x.webp 1x,
-            ./src/assets/images/auth/phone_desk@2x.webp 2x"
+          srcSet="/images/auth/phone_desk@1x.webp 1x,
+            /images/auth/phone_desk@2x.webp 2x"
           type="image/webp"
         />
         <source
           media="(min-width: 1440px)"
-          srcSet="./src/assets/images/auth/phone_desk@1x.png 1x,
-            ./src/assets/images/auth/phone_desk@2x.png 2x"
+          srcSet="/images/auth/phone_desk@1x.png 1x,
+            /images/auth/phone_desk@2x.png 2x"
           type="image/png"
         />
         <source
           sizes="(max-width: 767px)"
-          srcSet="./src/assets/images/auth/phone_mob@1x.webp 1x,
-            ./src/assets/images/auth/phone_mob@2x.webp 2x"
+          srcSet="/images/auth/phone_mob@1x.webp 1x,
+            /images/auth/phone_mob@2x.webp 2x"
           type="image/webp"
         />
         <source
           sizes="(max-width: 767px)"
-          srcSet="./src/assets/images/auth/phone_mob@1x.png 1x,
-            ./src/assets/images/auth/phone_mob@2x.png 2x"
+          srcSet="/images/auth/phone_mob@1x.png 1x,
+            /images/auth/phone_mob@2x.png 2x"
           type="image/png"
         />
         <img
-          src="./src/assets/images/auth/phone_desk@1x.png"
+          src="/images/auth/phone_desk@1x.png"
           alt="App Preview"
           loading="lazy"
           className="mx-auto"
